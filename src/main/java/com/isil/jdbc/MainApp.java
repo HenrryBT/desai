fa6c2f432532edf9c5469bd2d612cd8d9b6d502e
HenrryBT <henrry26bt@gmail.com>
2022-09-13 11:18:04 -0500

    Segunda sesion: jdbc para mantenimiento y prepared statements

--- src/main/java/com/isil/jdbc/MainApp.java
+++ src/main/java/com/isil/jdbc/MainApp.java
@@ -1,9 +1,6 @@
 package com.isil.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MainApp {
 
@@ -20,8 +17,35 @@ public class MainApp {
         // Crear el statement
         Statement st = con.createStatement();
 
-        // Ejecutar la sentencia SQL
-        ResultSet rs = st.executeQuery("SELECT * FROM Users");
+//        // Ejecutar la sentencia SQL
+//        ResultSet rs = st.executeQuery("SELECT * FROM Users");
+//
+//        // Insertar
+//        int filasAfectadas = st.executeUpdate("INSERT INTO Users VALUES (1,'Juan','984444459','Vancuver');");
+//
+//        // Update
+//        int filasAfectadas = st.executeUpdate("UPDATE Users SET name='Christian' WHERE name = 'Cueva';");
+//
+//        // Eliminar
+//        int filasAfectadas = st.executeUpdate("DELETE FROM Users WHERE idUser = 4;");
+//
+//        System.out.println("Filas afectadas: " + filasAfectadas);
+
+//        // Prepared statement para mantenimiento
+//        PreparedStatement preparedStatement = con.prepareStatement("insert into Users values(?, ?, ?, ?)");
+//        preparedStatement.setInt(1, 8);
+//        preparedStatement.setString(2, "Marta");
+//        preparedStatement.setString(3, "985454646");
+//        preparedStatement.setString(4, "Bogota");
+//
+//        int filasAfectadas = preparedStatement.executeUpdate();
+//        System.out.println("Filas afectadas: " + filasAfectadas);
+
+        // Prepared statement para consulta
+        PreparedStatement preparedStatement2 = con.prepareStatement("select * from Users where idUser = ?");
+        preparedStatement2.setInt(1, 8);
+
+        ResultSet rs = preparedStatement2.executeQuery();
 
         // Procesar el resultado
         while (rs.next()) {
